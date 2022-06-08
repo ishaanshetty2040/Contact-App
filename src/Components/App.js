fb05e412808d031204c01d8bcf896cb828654e79
Ishaan <ishaanshetty12@gmail.com>
2022-06-09 00:21:18 +0530

    Files modified

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,26 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
-  const contacts = [
-    {
-      id: "1",
-      name: "Ishaan",
-      email: "ishaanshetty12@gmail.com",
-    },
-    {
-      id: "2",
-      name: "Vasanth",
-      email: "vasanthshetty@hotmail.com.com",
-    },
-  ];
+  const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = (contact) => {
+    console.log(contact);
+  };
   return (
     <div>
       <Header />
-      <AddContact />
+      <AddContact addContactHandler={addContactHandler} />
       <ContactList contacts={contacts} /> {/* Giving property name */}
     </div>
   );
